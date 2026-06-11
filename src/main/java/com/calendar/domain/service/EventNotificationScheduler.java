009ed20779d4a2a4873e209a2c8a6010b76a6873
Loïc GEORGES <loic.georges@chanel.com>
2026-06-11 09:39:22 +0200

    Add logs on event sending checks

--- src/main/java/com/calendar/domain/service/EventNotificationScheduler.java
+++ src/main/java/com/calendar/domain/service/EventNotificationScheduler.java
@@ -4,12 +4,14 @@ import com.calendar.domain.model.Event;
 import com.calendar.domain.port.EventRepository;
 import com.calendar.domain.port.NotificationSender;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 import java.time.Clock;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @RequiredArgsConstructor
+@Slf4j
 public class EventNotificationScheduler {
 
     private final EventRepository repository;
@@ -18,11 +20,13 @@ public class EventNotificationScheduler {
 
     public void checkAndNotify() {
         LocalDateTime now = LocalDateTime.now(clock);
+        log.info("Checking events to be sent");
         List<Event> dueEvents = repository.findAllNotNotified()
                 .stream()
                 .filter(event -> event.isDue(now))
                 .toList();
 
+        log.info("Found {} events to be sent", dueEvents.size());
         dueEvents.forEach(this::notifyAndMark);
     }
 
